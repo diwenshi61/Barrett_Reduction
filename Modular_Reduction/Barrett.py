cd1e792dc999245b29992cec2d8b1da49d43a829
Diwen Shi <your_email@example.com>
2019-11-09 17:17:15 -0500

    inverse function

--- Modular_Reduction/Barrett.py
+++ Modular_Reduction/Barrett.py
@@ -1,6 +1,23 @@
 def check_power_of_two(n):
     return (not (n & (n-1))) and n
 
+def find_inverse(n, max_error):
+    i = 1
+    digits_found = 0
+    inverse = ""
+    numerator = 1
+    denominator = n
+    while digits_found < -max_error:
+        if (2 ** i) * numerator > denominator:
+            inverse += "1"
+            numerator = numerator * (2 ** i ) - denominator
+            denominator = denominator * (2 ** i)
+        else:
+            inverse += "0"
+        digits_found += 1
+        i += 1
+    return inverse
+
 if __name__ == "__main__":
     print("c = a mod b")
     try:
@@ -29,4 +46,7 @@ if __name__ == "__main__":
         quit()
     b_size = b.bit_length()
     max_error = -a_size
+    inv_b = find_inverse(b, max_error)
+    print(inv_b)
+    
     
